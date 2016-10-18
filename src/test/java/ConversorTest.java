a2d66edaabdb27f4cf8da0dd230f4e4dc0891b91
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-18 14:39:57 +0200

    Added unit test cases for Conversor class

---
+++ src/test/java/ConversorTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import otherclasses.Conversor;
+
+public class ConversorTest {
+	Conversor conversor = new Conversor();
+	Random rand = new Random();
+	
+	@Test
+	public void testToDegrees() {
+		double randDouble = 0.00;
+		for (int i = 0; i < 100; i++){
+			randDouble = rand.nextDouble()*2*Math.PI;
+			assertEquals(Math.toDegrees(randDouble), conversor.toDegrees(randDouble), 0.001);
+		}
+	}
+
+	@Test
+	public void testToRadians() {
+		double randDouble = 0.00;
+		for (int i = 0; i < 100; i++){
+			randDouble = rand.nextDouble()*360;
+			assertEquals(Math.toRadians(randDouble), conversor.toRadians(randDouble), 0.001);
+		}
+	}
+}
