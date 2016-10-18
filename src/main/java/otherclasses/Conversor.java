c1e1e3cb05eb4507af93e574a74f0a1d7849e4c3
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-18 12:18:32 +0200

    Initial commit

---
+++ src/main/java/otherclasses/Conversor.java
@@ -0,0 +1,13 @@
+package otherclasses;
+
+public class Conversor {
+
+	public double toDegrees(double radians){
+		return Math.toDegrees(radians);
+	}
+
+	public double toRadians(double degrees){
+		return Math.toRadians(degrees)
+;	}
+
+}
---
+++ src/main/java/otherclasses/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author carina
+ *
+ */
+package otherclasses;
