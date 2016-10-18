5d231244897087e1478ab7f46e1655855154fb80
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-18 13:40:18 +0200

    Added code for both buttons.

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -1,11 +1,25 @@
 package gui;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import otherclasses.Conversor;
 
 public class MainFrame {
 
-	private JFrame frame;
+	private JFrame frmConversor;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JButton btnConvertToDegrees = new JButton();
+	private JButton btnConvertToRadians = new JButton();
 
 	/**
 	 * Launch the application.
@@ -15,7 +29,7 @@ public class MainFrame {
 			public void run() {
 				try {
 					MainFrame window = new MainFrame();
-					window.frame.setVisible(true);
+					window.frmConversor.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -31,12 +45,76 @@ public class MainFrame {
 	}
 
 	/**
+	 * 
+	 */
+	private void onClickToRadiansButton(String degrees){
+		Conversor conversor = new Conversor();
+		NumberFormat df = new DecimalFormat("#.######");
+		double res = conversor.toRadians(Double.parseDouble(degrees));
+		textField_1.setText(df.format(res));
+	}
+	
+	/**
+	 * 
+	 */
+	private void onClickToDegreesButton(String radians){
+		Conversor conversor = new Conversor();
+		NumberFormat df = new DecimalFormat("#.######");
+		double res = conversor.toDegrees(Double.parseDouble(radians));
+		textField.setText(df.format(res));
+	}
+
+	
+	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmConversor = new JFrame();
+		frmConversor.getContentPane().setBackground(Color.GRAY);
+		frmConversor.getContentPane().setLayout(null);
+		
+		JLabel lblEnterTheValues = new JLabel("Value in degrees:");
+		lblEnterTheValues.setForeground(Color.WHITE);
+		lblEnterTheValues.setBounds(131, 91, 230, 15);
+		frmConversor.getContentPane().add(lblEnterTheValues);
+		
+		JLabel lblEnterTheValues_1 = new JLabel("Value in radians:");
+		lblEnterTheValues_1.setForeground(Color.WHITE);
+		lblEnterTheValues_1.setBounds(131, 132, 230, 15);
+		frmConversor.getContentPane().add(lblEnterTheValues_1);
+		
+		textField = new JTextField();
+		textField.setBounds(291, 87, 114, 19);
+		frmConversor.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(291, 128, 114, 19);
+		frmConversor.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		btnConvertToRadians.setText("Convert to radians");
+		btnConvertToRadians.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onClickToRadiansButton(textField.getText());
+			}
+		});
+		btnConvertToRadians.setToolTipText("Enter value in degrees and press here to convert to radians");
+		btnConvertToRadians.setBounds(461, 84, 175, 25);
+		frmConversor.getContentPane().add(btnConvertToRadians);
+		
+		btnConvertToDegrees.setText("Convert to degrees");
+		btnConvertToDegrees.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onClickToDegreesButton(textField_1.getText());
+			}
+		});
+		btnConvertToDegrees.setToolTipText("Enter value in radians and press here to convert to degrees");
+		btnConvertToDegrees.setBounds(461, 127, 175, 25);
+		frmConversor.getContentPane().add(btnConvertToDegrees);
+		frmConversor.setBackground(Color.WHITE);
+		frmConversor.setTitle("Convertor");
+		frmConversor.setBounds(100, 100, 804, 310);
+		frmConversor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
